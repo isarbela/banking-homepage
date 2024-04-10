d8405b890b3d8e22ec599534cdbe887674d1d832
isarbela <panzeref@gmail.com>
2024-04-10 18:52:58 -0300

    Implemented endpoints and added exception handler

--- src/main/java/me/dio/bankinghomepage/Application.java
+++ src/main/java/me/dio/bankinghomepage/Application.java
@@ -1,11 +1,16 @@
 package me.dio.bankinghomepage;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Application {
 
+@SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Banking Homepage API", version = "1.0", description = "User homepage information"),
+		servers = {@Server(url = "/", description = "Default server URL")})
+public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
