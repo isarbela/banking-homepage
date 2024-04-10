71f5162876d457fbf8e23d9e9d047c8dcdf85f69
isarbela <panzeref@gmail.com>
2024-04-10 09:48:36 -0300

    Initial Commit

---
+++ src/main/java/me/dio/bankinghomepage/BankingHomepageApplication.java
@@ -0,0 +1,13 @@
+package me.dio.bankinghomepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingHomepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingHomepageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/bankinghomepage/BankingHomepageApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.bankinghomepage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingHomepageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
